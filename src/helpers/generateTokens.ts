ddaa796754fc9142bfd2e9db8d8a06cfc9ce21b1
tgech71 <tgech71@gmail.com>
2023-03-20 11:37:15 +0300

    first commit 22

--- src/helpers/generateTokens.ts
+++ src/helpers/generateTokens.ts
@@ -4,7 +4,7 @@ export const generateAccessTokens = (claims: {
   metadata: { roles: Array<string>; user_id: string };
 }) => {
   const accessToken = jwt.sign(claims, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15s",
+    expiresIn: "15m",
   });
   return accessToken;
 };
@@ -13,7 +13,7 @@ export const generateRefreshTokens = (claims: {
   metadata: { roles: Array<string>; user_id: string };
 }) => {
   const refreshToken = jwt.sign(claims, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "1m",
+    expiresIn: "3d",
   });
   return refreshToken;
 };
@@ -40,8 +40,7 @@ export const generateNewAccessToken = async (refreshToken: string) => {
         const currentDate = Date.now();
         const refTokenExpDate = exp * 1000;
         // if dif is less than 1Day refresh the refreshToken
-        // if (refTokenExpDate - currentDate < 86400000)
-        if (refTokenExpDate - currentDate < 30000)
+        if (refTokenExpDate - currentDate < 86400000)
           newRefreshToken = generateRefreshTokens(claims);
       }
     }
