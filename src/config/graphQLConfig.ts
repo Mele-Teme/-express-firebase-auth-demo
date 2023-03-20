53f44ab254c735ebe218eb951449dab1f6d92efb
tgech71 <tgech71@gmail.com>
2023-03-20 09:56:10 +0300

    first commit 18

--- src/config/graphQLConfig.ts
+++ src/config/graphQLConfig.ts
@@ -2,5 +2,6 @@ import { GraphQLClient } from "graphql-request";
 export const graphQLClient = new GraphQLClient(process.env.HASURA_GRAPHQL_URL, {
   headers: {
     "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+    
   },
 });
