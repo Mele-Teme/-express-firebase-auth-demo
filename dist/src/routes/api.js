5db6ce1fda53525b05b37429af933660f6deefa2
tgech71 <tgech71@gmail.com>
2023-04-03 16:34:49 +0300

    check agai 2

--- dist/src/routes/api.js
+++ dist/src/routes/api.js
@@ -1,5 +1,5 @@
 import express from "express";
 import { registerSupplierController } from "../controllers/api.js";
 export const apiRouter = express.Router();
-apiRouter.post("/registerSupplier", registerSupplierController);
+apiRouter.get("/registerSupplier", registerSupplierController);
 //# sourceMappingURL=api.js.map
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Express } from "express";
 import { registerSupplierController } from "../controllers/api.js";
-export const apiRouter = express.Router();
 
-apiRouter.post("/registerSupplier", registerSupplierController);
+export const apiRouter = express.Router();
+apiRouter.get("/registerSupplier", registerSupplierController);
